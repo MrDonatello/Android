dd853069831b1294f7a80dd8ef8f957f4bb65bbf
MrDonatello <meet123666@gmail.com>
2021-07-25 14:11:33 +0600

    Update MainActivity.kt

--- udacity_2/app/src/main/java/com/anderysinitsyn/udacity_2/MainActivity.kt
+++ udacity_2/app/src/main/java/com/anderysinitsyn/udacity_2/MainActivity.kt
@@ -3,9 +3,12 @@ package com.anderysinitsyn.udacity_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-lateinit var button:Button
+
 
 class MainActivity : AppCompatActivity() {
+    
+    lateinit var button:Button
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
