991ba165ed58f0f0711880c20f16626ebb9526c8
Andrey <meet123666@gmail.com>
2021-07-25 14:27:44 +0600

    add 2 project

--- udacity_2/app/src/main/java/com/anderysinitsyn/udacity_2/MainActivity.kt
+++ udacity_2/app/src/main/java/com/anderysinitsyn/udacity_2/MainActivity.kt
@@ -2,15 +2,16 @@ package com.anderysinitsyn.udacity_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 
 class MainActivity : AppCompatActivity() {
     
-    lateinit var button:Button
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
 }
